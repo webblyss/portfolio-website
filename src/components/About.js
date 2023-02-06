ce8d873c52bb33b214b955984fadbaff69a70532
Coder <Codec@Issahakus-MacBook-Pro.local>
2023-02-06 12:48:17 +0000

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <div className="flex flex-col justify-center items-center">
                     <h4 className='underline text-3xl my-5'>About Me</h4>
                     <span className="px-3 text-justify text-sm">
-                        As a Full-Stack Developer, I have expertise in both front-end and back-end development, allowing me to build complete web applications from start to finish. My skills include HTML, CSS, JavaScript, React, Node.js, and experience with various databases like MongoDB and MySQL.
+                        As a Full-Stack Developer, I have expertise in both front-end and back-end development, allowing me to build complete web applications from start to finish.I am looking for a company where i can put my skills to work
                         <a download href="#CV" className="mx-3 underline text-blue-500">Download cv</a>
                     </span>
                     
