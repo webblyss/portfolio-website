d5eadfd25fa6c98092a920f50b3dfe108af076dc
solo <solo@solos-MacBook-Pro.local>
2023-02-23 12:28:01 -0800

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <img className='flex items-center justify-center mx-auto' src="https://t4.ftcdn.net/jpg/02/73/46/99/360_F_273469972_ESU9Rq3eIpSrK3xddlIEyDh7vrslbiGg.jpg" alt="" srcset="" />
                 <div className="flex flex-col justify-center items-center">
                     <h4 className='underline text-3xl my-5'>About Me</h4>
-                    <span className="px-3 text-justify text-sm">
+                    <span className="px-5 text-justify text-sm">
                         As a Full-Stack Developer, I have expertise in both front-end and back-end development, allowing me to build complete web applications from start to finish.I am looking for a company where i can put my skills to work
                         <a download href={CV}className="mx-3 underline text-blue-500">Download cv</a>
                     </span>
